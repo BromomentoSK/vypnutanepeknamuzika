2dbd78d82188c8a430c57a3ffa9eba885aecef13
BromomentoSK <67321488+BromomentoSK@users.noreply.github.com>
2021-02-07 20:01:17 +0100

    Finally fixed this old script
    changed __resource.lua to fxmanifest.lua and fixed some stuff

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'Bromomento'
+description 'Basic script that turns off music while flying in choppa or plane'
+version '69'
+
+-- What to run
+client_scripts {
+    'client.lua'
+}
